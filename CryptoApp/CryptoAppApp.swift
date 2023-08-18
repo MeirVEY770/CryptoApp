750afb86cffb5efc459d3bc24a79086b2ca8080f
Meir <eagudaev@gmail.com>
2023-08-18 14:14:38 +0300

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Meir on 18.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
