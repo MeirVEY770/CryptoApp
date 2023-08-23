fd77bc267222a0b693bb7dbe66c2e875002148b7
Meir <eagudaev@gmail.com>
2023-08-23 21:45:36 +0300

    Added SearchBarView

--- CryptoApp/Core/Home/Views/HomeView.swift
+++ CryptoApp/Core/Home/Views/HomeView.swift
@@ -14,6 +14,8 @@ struct HomeView: View {
             VStack {
                 homeHeader
                 
+                SearchBarView(searchText: $vm.searchText)
+                
                 columnTitles
                 
                 if !showPortfolio {
